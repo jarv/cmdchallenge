d5b8128acd99d96164516fb68a49edb7f5905698
John Jarvis <john@jarv.org>
2017-01-31 19:50:09 +0100

    Better handling of non-zero error codes, ordering for print_common_lines

--- bin/docker_cmd.py
+++ bin/docker_cmd.py
@@ -52,12 +52,15 @@ def output_from_cmd(cmd, challenge, docker_version=None, docker_base_url=None, t
     client = docker.DockerClient(version=docker_version, base_url=docker_base_url, tls=tls_config)
     b64cmd = b64encode(cmd)
     challenge_dir = path.join(WORKING_DIR, challenge['slug'])
-    docker_cmd = "cd {challenge_dir} && echo {b64cmd} | base64 -d > /tmp/script.sh && timeout {timeout} bash -O globstar /tmp/script.sh".format(
+    return_token = uuid.uuid4()
+    docker_cmd = "cd {challenge_dir} && echo {b64cmd} | base64 -d > /tmp/script.sh && timeout {timeout} bash -O globstar /tmp/script.sh; echo {return_token}$?".format(
         challenge_dir=challenge_dir,
         b64cmd=b64cmd,
-        timeout=CMD_TIMEOUT)
-    token = uuid.uuid4()
+        timeout=CMD_TIMEOUT,
+        return_token=return_token)
+
     if 'tests' in challenge:
+        token = uuid.uuid4()
         test_cmd = ""
         for t in challenge['tests']:
             test_cmd += "if ! {test}; then echo {token}{msg};fi\n".format(
@@ -71,23 +74,27 @@ def output_from_cmd(cmd, challenge, docker_version=None, docker_base_url=None, t
             timeout=CMD_TIMEOUT)
 
     docker_cmd = "bash -c '{}'".format(docker_cmd)
-    return_code = 1
+    return_code = -1
     test_errors = None
     with timeout(seconds=DOCKER_TIMEOUT):
         try:
             LOG.debug("Running `{}` in container".format(docker_cmd))
             output = client.containers.run('cmdline', docker_cmd, **DOCKER_OPTS)
-            test_errors = re.findall(r'{}(.*)'.format(token), output)
-            if test_errors:
-                output = re.sub(r'{}.*'.format(token), '', output, re.M).rstrip()
+            return_code_match = re.search(r'{}(\d+)'.format(return_token), output)
+            if return_code_match is None:
+                raise ValidationError("Unable to determine return code from command")
             else:
-                test_errors = None
-            return_code = 0
+                return_code = int(return_code_match.group(1))
+            output = re.sub(r'{}\d+'.format(return_token), '', output).rstrip()
+            if 'tests' in challenge:
+                test_errors_matches = re.findall(r'{}(.*)'.format(token), output)
+                if test_errors_matches:
+                    test_errors = test_errors_matches
+                    output = re.sub(r'{}.*'.format(token), '', output, re.M).rstrip()
         except SSLError as e:
             LOG.exception("SSL validation error connecting to {}".format(docker_base_url))
             raise ValidationError("SSL Error")
         except ContainerError as e:
-            output = re.sub(r'/tmp/script.sh: line \d+: (.*)', r'\1', e.stderr)
             return_code = e.exit_status
             if return_code == 124:
                 output += "\n** Command timed out after {} seconds **".format(CMD_TIMEOUT)
