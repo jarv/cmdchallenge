ccf81b25f56f31bb919d34bff1dc49bcd6195163
John Jarvis <john@jarv.org>
2023-03-08 16:15:46 +0100

    remove css that is no longer needed

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        'css/jquery.terminal.min.css'
-      ]
-    }
-  }
+  build: { }
 })
